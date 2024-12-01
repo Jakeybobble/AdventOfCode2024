acb32ef1217fc5266ba8544b47a14a470a76e4c9
Jacob G.S. <jacobgsdev@gmail.com>
2024-12-01 10:33:30 +0100

    Solved part one of day 1

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -11,5 +11,6 @@ class Program
     {
         var solver = SolverFactory.GetSolver(day);
         solver.Solve();
+        
     }
 }
--- AdventOfCode2024/Solvers/Day1.cs
+++ AdventOfCode2024/Solvers/Day1.cs
@@ -3,7 +3,40 @@ namespace AdventOfCode2024.Solvers {
     {
         public void Solve()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("** Wohoo! Day 1 of Advent of Code! **");
+
+            int total_distance = Parse("InputData/day1.txt");
+
+            Console.WriteLine($"The total distance is {total_distance}!");
+
+        }
+
+        private int Parse(string path)
+        {
+            int total_distance = 0;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            using(StreamReader file = new StreamReader(path)) {
+                string line;
+                while ((line = file.ReadLine()) != null) {
+                    string[] parts = line.Split("   ");
+                    left.Add(int.Parse(parts[0]));
+                    right.Add(int.Parse(parts[1]));
+                }
+                file.Close();
+            }
+
+            left.Sort();
+            right.Sort();
+
+            for(int i = 0; i < left.Count; i++) {
+                int distance = Math.Abs(left[i] - right[i]);
+                total_distance += distance;
+            }
+            return total_distance;
+
         }
     }
 }
