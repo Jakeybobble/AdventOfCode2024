65e3d20308f69262c40d12797c67178ab949e16d
Jacob G.S. <jacobgsdev@gmail.com>
2024-12-04 06:31:44 +0100

    Solved part two of day 2

--- AdventOfCode2024/Solvers/Day2.cs
+++ AdventOfCode2024/Solvers/Day2.cs
@@ -1,5 +1,8 @@
 namespace AdventOfCode2024.Solvers {
     public class Day2 : ISolver {
+
+        // I'd like to rewrite this one in a better way, possibly with more LINQ
+
         public void Solve() {
             Console.WriteLine("** Hooray! Day 2! **");
 
@@ -14,31 +17,54 @@ namespace AdventOfCode2024.Solvers {
 
             string[] lines = File.ReadAllLines(path);
             foreach(string line in lines) {
-                int[] values = line.Split(" ").Select(s => int.Parse(s)).ToArray();
-                
-                bool safe = true;
-                int dir = 0;
-                for(int i = 1; i < values.Length; i++) {
-                    int value = values[i];
-                    int last = values[i-1];
+                List<int> values = line.Split(" ").Select(s => int.Parse(s)).ToList();
 
-                    int difference = value - last;
-                    int sign = Math.Sign(difference);
-                    if(i == 1) { // If beginning of loop
-                        dir = sign;
-                    }
+                bool is_safe = true;
 
-                    if(sign != dir || Math.Abs(difference) > 3) {
-                        safe = false; break;
+                int error_index = get_bad_index(values);
+
+                if(error_index != -1) {
+                    //Console.WriteLine($"Error found at index {1+error_index} ({values[error_index]}) in {string.Join(" ", values)}!");
+                    bool fixed_it = false;
+                    for(int i = 0; i < values.Count; i++) {
+                        List<int> new_values = new List<int>(values);
+                        new_values.RemoveAt(i);
+                        if (get_bad_index(new_values) == -1) {
+                            //Console.WriteLine($"Fixed: {string.Join(" ", new_values)}");
+                            fixed_it = true;
+                            break;
+                        }
                     }
+                    if(!fixed_it) is_safe = false;
 
                 }
-                if(safe) safe_reports++;
 
+                if(is_safe) safe_reports++;
+                //Console.WriteLine();
 
             }
 
             return safe_reports;
+        }
+
+        // Returns index of bad value, otherwise -1
+        private int get_bad_index(List<int> values) {
+            int direction = 0;
+            for(int i = 1; i < values.Count; i++) {
+                (int val, int prev) = (values[i], values[i-1]);
+                int difference = val - prev;
+                if(i == 1) {
+                    direction = Math.Sign(difference);
+                }
+
+                if(Math.Abs(difference) > 3 || Math.Abs(difference) < 1 || Math.Sign(difference) != direction) {
+                    return i;
+                }
+
+            }
+            return -1;
+            
+
         }
     }
 }
